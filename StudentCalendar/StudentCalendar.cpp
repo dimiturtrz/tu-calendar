8311fa237cc7b66ee62938124793db16abffff1f
Dimitar <dimitar.trz@gmail.com>
2020-05-19 19:03:10 +0300

    removing debug from main

--- StudentCalendar/StudentCalendar.cpp
+++ StudentCalendar/StudentCalendar.cpp
@@ -10,19 +10,6 @@
 using namespace std;
 
 int main() {
-	/*Date date1(2000, Date::Month::January, 17);
-	cout << date1 << endl;
-	Student student1("georgi", 123456, date1, 4);
-	cout << student1<< endl;
-	cout << student1.studentAge(Date(2018, Date::Month::January, 10))<< endl;
-	StudentCollection studentCollection1;
-	for (int i = 0; i < 13; ++i) {
-		studentCollection1.append(student1);
-	}
-	cout << studentCollection1[5]<< endl;
-
-	cout << studentCollection1<< endl;*/
-
 	ifstream testFileStream("../test.txt");
 
 	StudentCollection studentCollection;
@@ -30,9 +17,4 @@ int main() {
 	cout << studentCollection;
 
 	testFileStream.close();
-
-	/*cout << endl;
-	for (int i = 0; i < studentCollection1.getSize(); ++i) {
-		cout << studentCollection1[i] << endl;
-	}*/
 }
